880bf2df3d009f5244aa4cd4f973331bc2bce252
Hendri <hendrisetiawardana@gmail.com>
2018-06-25 14:20:03 +0800

    Added onbackpressed
    Added onbackpressed

--- GetBooks/app/src/main/java/com/example/henry/getbooks/BooksListActivity.java
+++ GetBooks/app/src/main/java/com/example/henry/getbooks/BooksListActivity.java
@@ -1,11 +1,13 @@
 package com.example.henry.getbooks;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -100,6 +102,28 @@ public class BooksListActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_exit);
+        builder.setPositiveButton(R.string.dialog_yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                BooksListActivity.super.onBackPressed();
 
+            }
+        });
+        builder.setNegativeButton(R.string.dialog_no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialog != null){
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 }
